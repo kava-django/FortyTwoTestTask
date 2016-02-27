83d23005a68b54e3f277081a4d7d873614669fa5
pavel <kava.django@gmail.com>
2016-02-27 17:57:30 +0200

    ticket:1 add contact

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.hello.models import MyContacts
+
+class MyContactsAdmin(admin.ModelAdmin):
+	list_display = ['name', 'surname', 'email', 'skype']
+
+admin.site.register(MyContacts, MyContactsAdmin)
+
+
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from apps.hello.models import MyContacts
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {'contact': MyContacts.objects.get(id=1)})
